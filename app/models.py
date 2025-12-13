65bca1172c4fa2b13ddc04c8a7e4cfac1b869ea0
Santam Chakraborty <drsantam@gmail.com>
2025-12-13 17:15:09 +0530

    added docstrings to Patient and Diagnosis models and removed incomplete Stage model with TNMPrefixChoices

--- app/models.py
+++ app/models.py
@@ -9,6 +9,9 @@ class GenderChoices(models.TextChoices):
     OTHER = 'O', 'Other'
 
 class Patient(models.Model):
+    '''
+    Model to store information about Patient.
+    '''
     patient_uid = models.CharField(unique=True,max_length=255,verbose_name = "Patient Hospital Unique ID", help_text="Enter the patient's Hospital Unique ID")
     name = models.CharField(max_length=255,verbose_name = "Patient Name", help_text="Enter the patient's name")
     date_of_birth = models.DateField(verbose_name = "Date of Birth", help_text="Enter the patient's date of birth")
@@ -24,8 +27,10 @@ class Patient(models.Model):
     def __str__(self):
         return f"{self.name} ({self.patient_uid})"
 
-
 class Diagnosis(models.Model):
+    '''
+    Model to store information about the diagnosis of the patient.
+    '''
     patient = models.ForeignKey(Patient,on_delete=models.CASCADE,verbose_name = "Patient", help_text="Enter the patient's name")
     diagnosis = models.ForeignKey(LookupDiagnosis,on_delete=models.CASCADE,verbose_name = "Diagnosis", help_text="Enter the patient's diagnosis")
     date_of_diagnosis = models.DateField(verbose_name = "Date of Diagnosis", help_text="Enter the patient's date of diagnosis",null=True,blank=True)
@@ -37,22 +42,3 @@ class Diagnosis(models.Model):
         verbose_name = "Diagnosis"
         verbose_name_plural = "Diagnoses"
 
-class TNMPrefixChoices(models.TextChoices):
-    p = 'p', 'pathological'
-    c = 'c', 'clinical'
-    y = 'y', 'post neoadjuvant'
-    yp = 'yp', 'post neoadjuvant pathological'
-    yc = 'yc', 'post neoadjuvant clinical'
-    r = 'r', 'recurrent'
-    a = 'a', 'autopsy'
-    m = 'm', 'multifocal'
-
-
-
-class Stage(model.Models):
-    diagnosis = models.ForeignKey(Diagnosis,on_delete=models.CASCADE,verbose_name = "Diagnosis", help_text="Select the patient's diagnosis")
-    tnm_staging = models.BooleanField(verbose_name = "TNM or AJCC Staging System", help_text="Select if the staging system is TNM or AJCC",default=True)
-    tnm_t_stage_prefix = models.CharField(max_length=2,choices=TNMPrefixChoices.choices,verbose_name = "TNM T Stage Prefix", help_text="Select the TNM T Stage Prefix")
-    tnm_t_stage = models.CharField(max_length=2,verbose_name = "TNM T Stage", help_text="Select the TNM T Stage")
-    tnm_t_stage_suffix = models.CharField(max_length=2,verbose_name = "TNM T Stage Suffix", help_text="Select the TNM T Stage Suffix")
-    
